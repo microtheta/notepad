11f966668e1350d13cca8779eeb2a37a4892b9ba
Mahesh <mahesh@macbook-pro.local>
2018-12-23 14:00:50 +0530

    About page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './pages/Main';
+import AboutPage from './pages/About';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var botPattern = '(googlebot/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)';
+var re = new RegExp(botPattern, 'i');
+var userAgent = window.navigator.userAgent || '';
+if (re.test(userAgent)) {
+  ReactDOM.render(<AboutPage />, document.getElementById('root'));
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-if(process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'development') {
   import('@sentry/browser').then((Sentry) => {
     Sentry.init({ dsn: 'https://b1c05155a231405a991cd55f92b226ce@sentry.io/1356723' });
   }).catch(err => { console.log(err) });
---
+++ src/pages/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <div id="editable" placeholder="Type here..." data-gramm_editor="false" style={{ minHeight: '100%', outline: '0px', padding: '20px' }}>
+        <h1>Quick Notes - Online Notepad</h1>
+        <div><br /></div>
+        <div>This is a simple yet very powerful notepad.</div>
+        <div><br /></div>
+        <div>All your data is stored in your browser and this app works offline too!</div>
+        <div>So, anything you write in microtheta notepad gets backed up to your browser, so it’s always there even if you restart your browser or if it happens to crash.<br /></div>
+        <div><br /></div>
+        <div>Microtheta Notepad has got rich text editing support and all formatting options can be accessed using your keyboard, that's cool!</div>
+        <div>It’s free from distractions, works offline, and supports multiple notes. It's open source!<br /></div>
+        <div>
+          <div><br className="Apple-interchange-newline" />This app can simply replace sticky notes.</div>
+          <div><br /></div>
+          <div>My notes are always listed in recent order, that's good.</div>
+        </div>
+        <div><br /></div>
+      </div>
+    )
+  }
+}
